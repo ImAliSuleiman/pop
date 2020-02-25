2a8091f1ef56ad418883059d62cf739d2fb1dba4
Ali Suleiman <allysuley@gmail.com>
2020-02-25 16:50:05 +0300

    Navigate to second page from fourth page on back button press

--- lib/fourth.dart
+++ lib/fourth.dart
@@ -10,55 +10,58 @@ class _FourthPageState extends State<FourthPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Fourth Page'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Card(
-              color: Colors.grey[200],
-              elevation: 0,
-              margin: EdgeInsets.symmetric(horizontal: 24),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8)),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                  controller: _controller,
-                  keyboardType: TextInputType.text,
-                  textInputAction: TextInputAction.done,
-                  maxLines: 1,
-                  maxLength: 30,
-                  decoration: InputDecoration(
-                      icon: Icon(Icons.edit, color: Colors.grey),
-                      hintText: 'Write something here...',
-                      border: InputBorder.none,
-                      fillColor: Colors.grey[300],
-                      contentPadding: EdgeInsets.only(top: 8, bottom: 8),
-                      hintStyle: TextStyle(
-                        fontSize: 16.0,
-                        color: Colors.grey[500],
-                      ),
-                      counterText: ''),
+    return WillPopScope(
+      onWillPop: () => _popUntilSecond(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Fourth Page'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Card(
+                color: Colors.grey[200],
+                elevation: 0,
+                margin: EdgeInsets.symmetric(horizontal: 24),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8)),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextField(
+                    controller: _controller,
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.done,
+                    maxLines: 1,
+                    maxLength: 30,
+                    decoration: InputDecoration(
+                        icon: Icon(Icons.edit, color: Colors.grey),
+                        hintText: 'Write something here...',
+                        border: InputBorder.none,
+                        fillColor: Colors.grey[300],
+                        contentPadding: EdgeInsets.only(top: 8, bottom: 8),
+                        hintStyle: TextStyle(
+                          fontSize: 16.0,
+                          color: Colors.grey[500],
+                        ),
+                        counterText: ''),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _popUntilSecond,
+          tooltip: 'Return',
+          child: Icon(Icons.keyboard_return),
+        ), // This trailing comma makes auto-formatting nicer for build methods.
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _popUntilSecond,
-        tooltip: 'Return',
-        child: Icon(Icons.keyboard_return),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
-  _popUntilSecond() {
+  _popUntilSecond() async {
     // Pop until second page
     // Send data captured through text field to second page
     Navigator.popUntil(context, (route) {
